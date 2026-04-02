41f28135de87f22e433ecb02045a0795d794f55b
Studio71 <sarwararnob999@gmail.com>
2026-04-02 20:13:35 +0600

    Update App.jsx

--- studio-71-mern/frontend/src/App.jsx
+++ studio-71-mern/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from "./Home";
 import Invoice from './Invoice';
 
 function App() {
